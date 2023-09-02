b728f6b185b7f452f3fac6c4de631a8bd6aeb3f8
구유정 <81794810+guyujung@users.noreply.github.com>
2023-09-02 18:52:39 +0900

    Revert "Merge pull request #7 from SSUGraduationWork/feature/#6-contribution"
    This reverts commit 20aa0afc53cd00336041134b8801aac4ead89c3f.

--- src/app/Contribution/contributionRoute.js
+++
@@ -1,7 +0,0 @@
-const contribution = require("./contributionController");
-module.exports = (app) => {
-
-    // 1. 전체 기여도 조회 API
-    app.get('/contribution/:teamId', contribution.getContributions);
-
-};
